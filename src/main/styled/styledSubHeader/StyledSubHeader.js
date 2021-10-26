07e63714b8dbccb660ff7378acf2a9105fd1ddc1
Bayheck <67043367+Bayheck@users.noreply.github.com>
2021-10-26 13:02:31 +0600

    Separated subheader style component

---
+++ src/main/styled/styledSubHeader/StyledSubHeader.js
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+
+const StyledSubHeader = styled.div`
+    &{
+      max-width: 1224px;
+      width: 100%;
+      margin: 0 auto;
+    }
+  
+    & ul{
+      padding: 0;
+      font-size: 14px;
+      line-height: 17px;
+      margin: 20px 0 20px 281px;
+    }
+  
+    & ul li{
+      display: inline-block;
+      margin-right: 56px;
+      text-align: center;
+    }
+
+    & ul li:last-child {
+      margin-right: 0px;
+    }
+  
+    &&& ul > li > svg{
+      margin-bottom: 10px;
+    }
+`
+
+export default StyledSubHeader;
--- src/main/subheader/Subheader.js
+++ src/main/subheader/Subheader.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import StyledSubHeader from "../styled/styledSubHeader/StyledSubHeader";
 import { ReactComponent as ArrowsIcon } from '../../assets/Subheader/arrows.svg';
 import { ReactComponent as CardIcon } from '../../assets/Subheader/card.svg';
 import { ReactComponent as DiamondIcon } from '../../assets/Subheader/diamond.svg';
@@ -6,35 +6,6 @@ import { ReactComponent as HomeIcon } from '../../assets/Subheader/home.svg';
 import { ReactComponent as InsuranceIcon } from '../../assets/Subheader/insurance.svg';
 import { ReactComponent as LocationIcon } from '../../assets/Subheader/location.svg';
 import { ReactComponent as MoneyIcon } from '../../assets/Subheader/money.svg';
-
-const StyledSubHeader = styled.div`
-    &{
-      max-width: 1224px;
-      width: 100%;
-      margin: 0 auto;
-    }
-  
-    & ul{
-      padding: 0;
-      font-size: 14px;
-      line-height: 17px;
-      margin: 20px 0 20px 281px;
-    }
-  
-    & ul li{
-      display: inline-block;
-      margin-right: 56px;
-      text-align: center;
-    }
-
-    & ul li:last-child {
-      margin-right: 0px;
-    }
-  
-    &&& ul > li > svg{
-      margin-bottom: 10px;
-    }
-`
 
 const Subheader = () =>{
     return(
@@ -64,7 +35,6 @@ const Subheader = () =>{
                     <HomeIcon/>
                     <div>ForteFamily</div>
                 </li>
-
                 <li>
                     <LocationIcon/>
                     <div>Контакты</div>
